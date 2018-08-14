eb7500a19edbf6d1978f46d1bd6e7a04c3aa7559
mark sauer-utley <mark.utley.713@gmail.com>
2018-08-14 10:01:46 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const mappedMovies = (actor) => {
+    return actor.movies.map(movie => {
+      return <li>{movie}</li>
+    })
+  }
+
+  const mappedActors = actors.map(actor => {
+    return (
+      <div>
+        <h3>Name: {actor.name}</h3>
+        <p>Movies:</p>
+        <ul>
+          {mappedMovies(actor)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {mappedActors}
     </div>
   );
 };
